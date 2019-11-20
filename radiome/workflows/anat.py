f8e364dd09338b0b431f5e4ff81cf7757627b553
Anibal Solon <anibalsolon@gmail.com>
2019-11-20 15:20:25 -0600

    testing it for a PR

---
+++ radiome/workflows/anat.py
@@ -0,0 +1,15 @@
+from ..resource_pool import ResourcePool, ResourceKey
+
+# TODO input validation by resouce metadata (variant, flags, type)
+# TODO output validation by resouce metadata (variant, flags, type)
+# TODO config schema validator
+
+inputs = [
+    ResourceKey()
+]
+
+def create_workflow(workflow, configuration, resource_pool: ResourcePool):
+
+    inputs = []
+    outputs = []
+
