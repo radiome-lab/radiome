8c412a719adae13a082c4ef84a87a97a0cfe386a
Pu Zhao <puz474494@gmail.com>
2019-11-26 16:54:30 -0600

    add dependency & fix ci

--- tests/test_create_workflow.py
+++ tests/test_create_workflow.py
@@ -8,6 +8,9 @@ class TestCreateWorkflow(TestCase):
     def test_create_workflow(self):
         wf = pe.Workflow(name='mock')
         self.assertTrue(anat_preproc.create_workflow(wf, {'n4': True, 'denoise': True}, {}))
+        self.assertTrue(anat_preproc.create_workflow(wf, {'n4': True, 'denoise': False}, {}))
+        self.assertTrue(anat_preproc.create_workflow(wf, {'n4': False, 'denoise': True}, {}))
+        self.assertTrue(anat_preproc.create_workflow(wf, {'n4': False, 'denoise': False}, {}))
 
         # test invalid config
         with self.assertRaises(ValueError):
