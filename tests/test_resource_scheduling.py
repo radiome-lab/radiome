6ad88a0362a8da16a9c679040b628c0d4a2f5029
Anibal Solon <anibalsolon@gmail.com>
2020-01-27 13:50:36 -0500

    really cleaning up

--- tests/test_resource_scheduling.py
+++ tests/test_resource_scheduling.py
@@ -1,71 +1,11 @@
-from unittest import TestCase
 import logging
-import time
 import tempfile
-from distributed import Client, LocalCluster, Lock, get_client, progress, get_worker
+import time
 from multiprocessing import Process, freeze_support
+from unittest import TestCase
 
-
-
-# class While():
-
-#     def __init__(self, condition, body, max_iterations=-1):
-#         self.condition = condition
-#         self.body = body
-#         self.max_iterations = max_iterations
-
-#     def __call__(self):
-
-#         client = get_client()
-
-#         return_value = None
-
-#         iteration = 0
-#         while self.max_iterations == -1 or iteration < self.max_iterations:
-#             condition = bool(client.submit(
-#                 self.condition,
-#                 previous=return_value,
-#                 iteration=iteration,
-#                 pure=False
-#             ).result())
-
-#             logging.warning("Iteration %d: condition is %s & previous result is %s", iteration, str(condition), str(return_value))
-
-#             if not condition:
-#                 break
-
-#             return_value = client.submit(
-#                 self.body,
-#                 previous=return_value,
-#                 iteration=iteration,
-#                 pure=False
-#             ).result()
-
-#             iteration += 1
-
-#         return return_value
-
-
-# def condition(iteration, previous=None):
-#     if previous is None:
-#         return True
-#     if previous < 20:
-#         return True
-#     return False
-
-# def body(iteration, previous=None):
-#     if previous == 1:
-#         return previous + 1
-#     if previous is not None:
-#         return previous + iteration
-#     return 0
-
-# while_node = While(condition, body, max_iterations=10)
-
-# futures = [client.submit(while_node)]
-
-# results = client.gather(futures)
-# print(results)
+from distributed import (Client, LocalCluster, Lock, get_client, get_worker,
+                         progress)
 
 if __name__ == '__main__':
 
